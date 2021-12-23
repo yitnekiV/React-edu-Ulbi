2058febc100d438c5580eeaa30d94da487b11521
Vikentii Palyha <vikentii_palyha@epam.com>
2021-12-23 11:29:21 +0200

    Decompose, + custom hooks

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import PostForm from './components/PostForm';
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePost";
 
 
 
@@ -33,17 +34,7 @@ function App() {
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    console.log('Get posts');
-    if(filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase()))
-  }, [ filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
