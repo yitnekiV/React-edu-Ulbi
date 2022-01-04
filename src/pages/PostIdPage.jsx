86a2383a8e20e3bc4f82f78742426861f8980979
Vikentii Palyha <vikentii_palyha@epam.com>
2022-01-04 13:10:19 +0200

    Add comments to post page

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,20 @@ import { useFetching } from '../hooks/useFetching';
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching( async (id) => {
         const response = await PostService.getById(id);
         setPost(response.data);
     });
 
+    const [fetchComments, isComLoading, comError] = useFetching( async (id) => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data);
+    });
+
     useEffect(() => {
         fetchPostById(params.id);
+        fetchComments(params.id);
     }, []);
     
     return (
@@ -23,7 +30,18 @@ const PostIdPage = () => {
                 ? <Loader/>
                 : <div>{post.id}. {post.title}</div>
             }
-            
+            <h1>Comments</h1>
+            {isComLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comm => 
+                        <div style={{padding: '15px 0'}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
+            }
         </div>
     )
 }
