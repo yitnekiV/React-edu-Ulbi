5bb86dc4e5c3d298ee19d806659bb7e3e6a19131
Vikentii Palyha <vikentii_palyha@epam.com>
2022-01-04 12:53:48 +0200

    Post page content

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/Loader/Loader';
+import { useFetching } from '../hooks/useFetching';
 
 const PostIdPage = () => {
+    const params = useParams();
+    const [post, setPost] = useState({});
+    const [fetchPostById, isLoading, error] = useFetching( async (id) => {
+        const response = await PostService.getById(id);
+        setPost(response.data);
+    });
+
+    useEffect(() => {
+        fetchPostById(params.id);
+    }, []);
+    
     return (
         <div>
-            <h1>Страница поста.</h1>
+            <h1>Страница поста. ID = {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div>{post.id}. {post.title}</div>
+            }
+            
         </div>
     )
 }
